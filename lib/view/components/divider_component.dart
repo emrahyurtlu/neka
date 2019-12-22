771295ac28f67e48e448cd6cef26ee76e750ad8f
Emrah Yurtlu <emrahyurtlu@gmail.com>
2019-12-22 13:18:53 +0300

    Yeni kart tasarımına başlandı. iPhone üzerinde test yapıldı.

---
+++ lib/view/components/divider_component.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:neka/settings/colors.dart';
+
+class DividerComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      width: double.infinity,
+      color: ColorLight,
+      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+    );
+  }
+}
--- lib/view/components/product_component.dart
+++ lib/view/components/product_component.dart
@@ -4,6 +4,7 @@ import 'package:neka/business/favorite_service.dart';
 import 'package:neka/settings/colors.dart';
 import 'package:neka/settings/font_families.dart';
 import 'package:neka/utils/dimension_util.dart';
+import 'package:neka/view/components/divider_component.dart';
 import 'package:neka/view/components/property_component.dart';
 
 class ProductComponent extends StatefulWidget {
@@ -45,163 +46,240 @@ class _ProductComponentState extends State<ProductComponent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 100,
-      margin: EdgeInsets.only(top: 5, bottom: 5, right: 0, left: 0),
-      padding: EdgeInsets.all(0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),
-          border: Border.all(color: Color.fromRGBO(241, 241, 241, 1)),
-          color: ColorWhite),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Wrap(
-            children: <Widget>[
-              //Image Section
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: CachedNetworkImage(
-                          imageUrl: widget.image,
-                          width: 80,
-                          height: 80,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) =>
-                              CircularProgressIndicator(),
-                          errorWidget: (context, url, error) =>
-                              Icon(Icons.error),
-                        )),
-                  )
-                ],
-              ),
-
-              //Info Section
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  //Title Section
-                  Container(
-                    width: DimensionUtil(
-                        context, EdgeInsets.only(top: 5, left: 10))
-                        .setWidthRel(subtract: 100),
-                    height: 30,
-                    padding: EdgeInsets.only(top: 5, left: 10),
-                    child: Stack(
+        width: double.infinity,
+        height: 170,
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),
+            border: Border.all(color: ColorLight),
+            color: ColorWhite),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //TopRow
+            Row(
+              children: <Widget>[
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 40,
+                    height: 40,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Emrah Yurtlu',
+                          style: TextStyle(
+                              fontFamily: FontFamily.AvenirMedium,
+                              fontSize: 15,
+                              color: ColorDark)),
+                      Text('18.12.2019 12:36',
+                          style: TextStyle(
+                              fontFamily: FontFamily.AvenirLight,
+                              fontSize: 12,
+                              color: ColorDark)),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                PopupMenuButton<String>(
+                    icon: Icon(
+                      Icons.more_vert,
+                      color: ColorText,
+                    ),
+                    child: ListTile(
+                      leading: IconButton(
+                        icon: Icon(Icons.add_alarm),
+                        onPressed: () {
+                          print('Hello world');
+                        },
+                      ),
+                      title: Text('Title'),
+                      trailing: Icon(Icons.account_circle),
+                    ),
+                    itemBuilder: (BuildContext context) {
+                      return <PopupMenuEntry<String>>[
+                        const PopupMenuItem<String>(
+                          value: 'Value1',
+                          child: Text('Choose value 1'),
+                        ),
+                        const PopupMenuItem<String>(
+                          value: 'Value2',
+                          child: Text('Choose value 2'),
+                        ),
+                        const PopupMenuItem<String>(
+                          value: 'Value3',
+                          child: Text('Choose value 3'),
+                        ),
+                      ];
+                    }),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 5, bottom: 5),
+              child: DividerComponent(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Wrap(
+                  children: <Widget>[
+                    //Image Section
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
                         GestureDetector(
                           onTap: widget.onTap,
-                          child: Text(
-                            widget.title,
-                            maxLines: 2,
-                            style: TextStyle(
-                                fontFamily: FontFamily.AvenirMedium,
-                                fontSize: 15,
-                                color: ColorHeader),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                                imageUrl: widget.image,
+                                width: 75,
+                                height: 75,
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) =>
+                                    CircularProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
+                              )),
+                        )
+                      ],
+                    ),
+
+                    //Info Section
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        //Title Section
+                        Container(
+                          width: DimensionUtil(
+                                  context, EdgeInsets.only(top: 5, left: 5))
+                              .setWidthRel(subtract: 100),
+                          height: 30,
+                          padding: EdgeInsets.only(top: 5, left: 10),
+                          child: Stack(
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: widget.onTap,
+                                child: Text(
+                                  widget.title,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                      fontFamily: FontFamily.AvenirMedium,
+                                      fontSize: 15,
+                                      color: ColorHeader),
+                                ),
+                              ),
+                              //Favorite Button
+                              /*Positioned(
+                              width: 30,
+                              height: 30,
+                              child: Container(
+                                color: Colors.white,
+                                child: IconButton(
+                                  icon: favIcon,
+                                  iconSize: 24,
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: _favOnPress,
+                                  color: ColorPrimary,
+                                ),
+                              ),
+                              top: 0,
+                              right: 0,
+                            ),*/
+                            ],
                           ),
                         ),
-                        //Favorite Button
-                        Positioned(
-                          width: 30,
-                          height: 30,
+                        GestureDetector(
+                          onTap: widget.onTap,
                           child: Container(
-                            color: Colors.white,
-                            child: IconButton(
-                              icon: favIcon,
-                              iconSize: 24,
-                              padding: EdgeInsets.all(0),
-                              onPressed: _favOnPress,
-                              color: ColorPrimary,
+                            padding: EdgeInsets.only(left: 10),
+                            width: 304,
+                            child: Wrap(
+                              direction: Axis.horizontal,
+                              alignment: WrapAlignment.start,
+                              runSpacing: 0,
+                              spacing: 3,
+                              children: <Widget>[
+                                Image.asset('assets/images/del/bim_logo.png',
+                                    width: 30),
+                                Text(
+                                  'Bim/Yeşiltepe',
+                                  style: TextStyle(
+                                      fontFamily: FontFamily.AvenirBook,
+                                      fontSize: 12,
+                                      color: ColorText),
+                                )
+                              ],
                             ),
                           ),
-                          top: 0,
-                          right: 0,
                         ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Container(
-                      padding: EdgeInsets.only(left: 10),
-                      width: 304,
-                      child: Wrap(
-                        direction: Axis.horizontal,
-                        alignment: WrapAlignment.start,
-                        runSpacing: 0,
-                        spacing: 3,
-                        children: <Widget>[
-                          Image.asset('assets/images/del/bim_logo.png',
-                              width: 30),
-                          Text(
-                            'Bim/Yeşiltepe',
-                            style: TextStyle(
-                                fontFamily: FontFamily.AvenirBook,
-                                fontSize: 12,
-                                color: ColorText),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Container(
-                      padding: EdgeInsets.only(left: 10),
-                      width: 304,
-                      child: Wrap(
-                        direction: Axis.horizontal,
-                        alignment: WrapAlignment.start,
-                        runSpacing: 0,
-                        spacing: 3,
-                        children: <Widget>[
-                          PropertyComponent(
-                              iconName: 'location_on', content: 'Mesafe 3km'),
-                          PropertyComponent(
-                              iconName: 'access_time',
-                              content: 'Açık',
-                              color: ColorGreen),
-                        ],
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10, right: 5),
-                          child: Text(
-                            '36,90TL',
-                            style: TextStyle(
-                                fontFamily: FontFamily.AvenirHeavy,
-                                fontSize: 16,
-                                color: ColorPriceDel,
-                                decoration: TextDecoration.lineThrough),
+                        GestureDetector(
+                          onTap: widget.onTap,
+                          child: Container(
+                            padding: EdgeInsets.only(left: 10),
+                            width: 304,
+                            child: Wrap(
+                              direction: Axis.horizontal,
+                              alignment: WrapAlignment.start,
+                              runSpacing: 0,
+                              spacing: 3,
+                              children: <Widget>[
+                                PropertyComponent(
+                                    iconName: 'location_on',
+                                    content: 'Mesafe 3km'),
+                                PropertyComponent(
+                                    iconName: 'access_time',
+                                    content: 'Açık',
+                                    color: ColorGreen),
+                              ],
+                            ),
                           ),
                         ),
-                        Text(
-                          '32,25TL',
-                          style: TextStyle(
-                              fontFamily: FontFamily.AvenirHeavy,
-                              fontSize: 16,
-                              color: ColorPrimary),
-                        ),
+                        GestureDetector(
+                          onTap: widget.onTap,
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 10, right: 5),
+                                child: Text(
+                                  '36,90TL',
+                                  style: TextStyle(
+                                      fontFamily: FontFamily.AvenirHeavy,
+                                      fontSize: 16,
+                                      color: ColorPriceDel,
+                                      decoration: TextDecoration.lineThrough),
+                                ),
+                              ),
+                              Text(
+                                '32,25TL',
+                                style: TextStyle(
+                                    fontFamily: FontFamily.AvenirHeavy,
+                                    fontSize: 16,
+                                    color: ColorPrimary),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
-    );
+                    )
+                  ],
+                ),
+              ],
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 5, bottom: 5),
+              child: DividerComponent(),
+            ),
+          ],
+        ));
   }
 
   void _favOnPress() async {
